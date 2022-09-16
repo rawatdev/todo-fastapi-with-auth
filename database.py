dcb19a92e26fc3844c75e70235ce16861898b801
Abhishek <abhishekrawat708090@gmail.com>
2022-09-16 13:27:36 +0530

    Add production postgres

--- database.py
+++ database.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+import os
+
 # Sqlite3
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
@@ -9,7 +11,13 @@ from sqlalchemy.ext.declarative import declarative_base
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
 
 # PostgreSQL
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:3480@localhost/TodoApplicationDatabase"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:3480@localhost/TodoApplicationDatabase"
+
+# PostgreSQL Prduction
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+if (SQLALCHEMY_DATABASE_URL.startswith("postgres://")):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace(
+        "postgres://", "postgresql://", 1)
 
 # Sqlite3
 # engine = create_engine(
