1810e766df8d79362930b92ece2a2e19a52b1bd7
Abhishek <abhishekrawat708090@gmail.com>
2022-09-16 13:56:02 +0530

    fix postresql production setup

--- database.py
+++ database.py
@@ -11,13 +11,13 @@ import os
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
 
 # PostgreSQL
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:3480@localhost/TodoApplicationDatabase"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:3480@localhost/TodoApplicationDatabase"
 
 # PostgreSQL Prduction
-# SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
-# if (SQLALCHEMY_DATABASE_URL.startswith("postgres://")):
-#     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace(
-#         "postgres://", "postgresql://", 1)
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+if (SQLALCHEMY_DATABASE_URL.startswith("postgres://")):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace(
+        "postgres://", "postgresql://", 1)
 
 # Sqlite3
 # engine = create_engine(
